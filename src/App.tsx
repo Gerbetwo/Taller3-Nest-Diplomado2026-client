a1f188703be24e915b2f87f21b7dcb29163b5fcc
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-01-31 10:59:18 -0500

    feat(Customers.Api): Fully Implement Minipos-Api

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@
 //  import reactLogo from './assets/react.svg'
 //  import viteLogo from '/vite.svg'
 import './App.css'
+import CustomersPage from "./pages/CustomersPage";
 
 function App() {
-  
+  return (<CustomersPage />)
 
-  return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-sky-400 drop-shadow-lg">
-        ¡Tailwind funcionando!
-      </h1>
-    </div>
-  )
+  // return (
+  //   <body className="min-h-screen bg-slate-900 flex items-center justify-center">
+  //     <script src="https://cdn.tailwindcss.com"></script>
+  //     <h1 className="text-4xl font-bold text-sky-400 drop-shadow-lg">
+  //       ¡Tailwind funcionando!
+  //     </h1>
+  //     <div className="flex items-center justify-center">
+  //       <a href="https://github.com/Gerbetwo" target="_blank"
+  //         className="flex items-center p-6 bg-slate-750 border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition-shadow duration-300 max-w-sm group">
+
+  //         <img className="w-20 h-20 rounded-full border-2 border-indigo-500 p-0.5" src="https://github.com/Gerbetwo.png" alt="GitHub Profile"></img>
+
+  //           <div className="ml-4">
+  //             <h3 className="text-xl font-bold text-gray-900 group-hover:text-indigo-600 transition-colors">
+  //               Gebertwo
+  //             </h3>
+  //             <p className="text-sm text-gray-600">
+  //               I'm a developer building cool things on GitHub. Check out my repositories!
+  //             </p>
+  //           </div>
+  //       </a>
+  //     </div>
+  //   </body>
+  // )
 
   //  const [count, setCount] = useState(0)
   //  return (
