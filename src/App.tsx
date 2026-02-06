8f0dba9057c0c781d38ca2f34be0ef9360f8d9a3
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-02-05 19:11:30 -0500

    feat: Add Queries funtionalitiys and updates CustomersPages to use it

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@
 //  import reactLogo from './assets/react.svg'
 //  import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import CustomersPage from "./pages/CustomersPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
 
 function App() {
   return (<CustomersPage />)
